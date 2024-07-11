bf0f0dfe6500643c538bdb7a591da18e37529564
MegiBlloshmi <megiblloshmi200313@gmail.com>
2024-07-11 13:44:02 +0200

    update 2

--- app/models.py
+++ app/models.py
@@ -1,11 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Kategori(models.Model):
+    kategori_name = models.CharField(max_length=300, null=True, blank=True)       
+    kategori_image = models.ImageField(upload_to='about/', null=True, blank=True)
+    kategori_slug = models.SlugField(unique=True, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.kategori_name}"
+
+
 class Product(models.Model):
     product_name = models.CharField(max_length=200, null=True, blank=True)
     product_description = models.TextField(max_length=1000, null=True, blank=True)
     product_price = models.DecimalField( max_digits=5, decimal_places=2, null=True, blank=True)
     product_image = models.ImageField(upload_to='product/', null=True, blank=True)
+    product_category = models.ForeignKey(Kategori, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"{self.product_name}"
+
+
+class Contact(models.Model):
+    contact_name = models.CharField(max_length=200, null=True, blank=True)
+    contact_surname = models.CharField(max_length=200, null=True, blank=True)
+    contact_email = models.EmailField(null=True, blank=True)
+    contact_description = models.TextField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.contact_name} {self.contact_surname}"
--- app/views.py
+++ app/views.py
@@ -4,22 +4,52 @@ from .models import *
 # Create your views here.
 def index(request):
     products = Product.objects.all()
-    context = {"products" : products}
+    kategories = Kategori.objects.all()
+    context = {"products" : products, "kategories" : kategories}
     return render(request, 'home.html', context)
 
-def about (request):
-    return render(request, 'about.html')
+def about(request):
+    kategori = Kategori.objects.all()
+    context = {"kategori" : kategori}
+    return render(request, 'about.html', context)
 
-def products (request):
+def products(request):
     products = Product.objects.all()
     context = {"products" : products}
     return render(request, 'products.html', context)
 
-def about (request):
-    return render(request, 'contact.html')
+def contact(request):
+    kategories = Kategori.objects.all()
+    if request.method == "POST":
+        name = request.POST["firstname"]
+        surname = request.POST["lastname"]
+        email = request.POST["email"]
+        comment = request.POST["comment"]
 
-def about (request):
+        Contact(
+            contact_name= name,
+            contact_surname = surname,
+            contact_email = email,
+            contact_description = comment
+        ).save()
+    context = {"kategories" : kategories}
+    return render(request, 'contact.html', context)
+
+def skincare(request):
     return render(request, 'skincare.html')
 
-def about (request):
+def haircare(request):
     return render(request, 'haircare.html')
+
+def detailproduct(request, id):
+    productDetail = Product.objects.get(pk=id)
+    kategories = Kategori.objects.all()
+    context = {"productDetail": productDetail, "kategories" : kategories}
+    return render(request, 'detailproduct.html', context) 
+
+def detailkategori(request, slug):
+    kategoriDetail = Kategori.objects.get(kategori_slug=slug)
+    kategories = Kategori.objects.all()
+    kategoriProducts = Product.objects.filter(product_category=kategoriDetail)
+    context = {"kategoriDetail": kategoriDetail, "kategories" : kategories, "kategoriProducts" : kategoriProducts}
+    return render(request, 'detailkategori.html', context) 
